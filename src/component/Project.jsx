9bf2acb5a08861c1e82edc03c975dbe6618a5740
Mim <fatemamim28@gmail.com>
2023-01-28 10:43:25 +0600

    ok

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -33,13 +33,13 @@ const Project = () => {
                                     
                                     <img src={images} alt="" className='h-full w-full'/>
                                     <div className='absolute top-16 w-full py-10 bg-color2 text-center space-x-8 hidden group-hover:block'>
-                                        <a href={codelink}>
+                                        <a href={codelink} target="_blank" rel="noreferrer">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 inline-block stroke-slate-400 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                             </svg>
                                         </a>
-                                        <a href={livelink}>
+                                        <a href={livelink} target="_blank" rel="noreferrer">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block stroke-slate-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
                                             </svg>
