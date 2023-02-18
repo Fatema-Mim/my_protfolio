bb7ccd007300aec74be044dfeae4a094e9eb7142
Mim <fatemamim28@gmail.com>
2023-02-18 11:52:19 +0600

    ok

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full md:h-screen bg-[#0a192f] flex justify-center items-center p-4 pb-28 md:pb-0'>
+    <div name='contact' className='w-full  pt-0 md:pt-28 bg-[#0a192f] flex justify-center items-center p-4 pb-28 md:pb-0'>
         <form method='POST' action="https://getform.io/f/a793803e-ad66-42e9-94af-261b44884792" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -9,7 +9,7 @@ const Work = () => {
     //setProject(data);
   
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f] pb-28 md:pb-0'>
+    <div name='work' className='w-full pt-0 md:pt-28 text-gray-300 bg-[#0a192f] pb-28 md:pb-0'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
