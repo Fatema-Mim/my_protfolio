7a5f73912ca2ce251338655c90fe48326cb04a3f
fatema-mim <fatemamim28@gmail.com>
2025-07-16 22:04:10 +0600

    new project add

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,65 +1,69 @@
-import React from 'react';
+import React from "react";
 import { data } from "../data/data.js";
 
-
 const Work = () => {
+  const project = data;
 
-    // projects file
-    const project = data;
-    //setProject(data);
-  
   return (
-    <div name='work' className='w-full pt-0 md:pt-28 text-gray-300 bg-[#0a192f] pb-28 md:pb-0'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+    <div
+      name="work"
+      className="w-full pt-0 md:pt-28 text-gray-300 bg-[#0a192f] pb-28 md:pb-0"
+    >
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="inline text-4xl font-bold text-gray-300 border-b-4 border-pink-600">
             Work
           </p>
-          <p className='py-6'> Check out some of my recent work</p>
+          <p className="py-6">Check out some of my recent work</p>
         </div>
 
-{/* container for projects */}
-<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          
-          {/* Gird Item */}
+        {/* container for projects */}
+        <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3">
           {project.map((item, index) => (
-  <div
-    key={index}
-    style={{ backgroundImage: `url(${item.image})` }}
-    className="shadow-lg shadow-[#040c16] group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-div "
-  >
-    {/* Hover effect for images */}
-    <div className="opacity-0 group-hover:opacity-100 ">
-      <span className="text-2xl font bold text-white tracking-wider ">
-        {item.name}
-      </span>
-      <div className="pt-8 text-center ">
-        {/* eslint-disable-next-line */}
-        <a href={item.github} target="_blank">
-          <button
-            className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
-          >
-            Code
-          </button>
-        </a>
-        {/* eslint-disable-next-line */}
-        <a href={item.live} target="_blank">
-          <button
-            className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
-          >
-            Live
-          </button>
-        </a>
-      </div>
-    </div>
-  </div>
-))}
+            <div
+              key={index}
+              className="relative group shadow-lg shadow-[#040c16] rounded-md overflow-hidden"
+            >
+              {/* Project image */}
+              <img
+                src={item.image}
+                alt="project"
+                className="object-cover w-full h-48 duration-300 transform group-hover:scale-110"
+              />
 
-
-</div>
+              {/* Hover overlay */}
+              <div className="absolute inset-0 flex flex-col items-center justify-center transition duration-300 bg-black opacity-0 bg-opacity-60 group-hover:opacity-100">
+                <div className="text-center">
+                  {/* Buttons */}
+                  <div className="flex flex-wrap justify-center">
+                    {item.github && (
+                      <a
+                        href={item.github}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="m-2"
+                      >
+                        <button className="px-4 py-2 text-sm font-bold text-gray-700 bg-white rounded-lg">
+                          Code
+                        </button>
+                      </a>
+                    )}
+                    <a
+                      href={item.live}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="m-2"
+                    >
+                      <button className="px-4 py-2 text-sm font-bold text-gray-700 bg-white rounded-lg">
+                        Live
+                      </button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -1,33 +1,31 @@
+import exahike2 from '../Image/exahike2.png';
+import molecule from '../Image/molecule.png';
+import expenses from '../Image/expenses.png';
+import travel from '../Image/travel.png';
 
-export const data =[
-    {
-        id:1,
-        image:"./images/project/food-network-HTML1.png",
-        github:"https://github.com/Fatema-Mim/food-network-a1",
-        live:"https://fatema-mim.github.io/food-network-a1/",
-    },
-    {
-        id:4,
-        image:"./images/project/js2.png",
-        github:"https://github.com/Fatema-Mim/fancy-dress",
-        live:"https://upbeat-wozniak-77bd4d.netlify.app/",
-    },
-    {
-        id:5,
-        image:"./images/project/js3.png",
-        github:"https://github.com/Fatema-Mim/cheesy_bite",
-        live:"https://jolly-lovelace-207855.netlify.app/",
-    },
-    {
-        id:6,
-        image:"./images/project/react1.png",
-        github:"https://github.com/Fatema-Mim/medi-help",
-        live:"https://medi-help-a8760.web.app/home",
-    },
-    {
-        id:7,
-        image:"./images/project/react2.png",
-        github:"https://github.com/Fatema-Mim/learning-hub",
-        live:"https://lucid-almeida-458662.netlify.app/home",
-    },
-]
+export const data = [
+  {
+    id: 1,
+    image: exahike2,
+    github: "",
+    live: "https://www.exahike.com/",
+  },
+  {
+    id: 4,
+    image: molecule,
+    github: "",
+    live: "https://www.moleculedata.com/",
+  },
+  {
+    id: 5,
+    image: expenses,
+    github: "https://github.com/Fatema-Mim/expense-frontend",
+    live: "https://expensetrack2.netlify.app",
+  },
+  {
+    id: 6,
+    image: travel,
+    github: "https://github.com/Fatema-Mim/travel",
+    live: "https://travel-agency-2.netlify.app/",
+  }
+];
